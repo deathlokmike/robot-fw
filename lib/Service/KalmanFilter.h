2baf0a27dda3c80f6aa9f1cacdf46ec03a6884bd
Mikhail Drozdov <deathlokmike@gmail.com>
2025-04-11 21:50:45 +0300

    Add Kalman filter

---
+++ lib/Service/KalmanFilter.h
@@ -0,0 +1,26 @@
+struct State {
+    double distanceFront;
+    double distanceSide;
+    double distanceHall;
+    double angle;
+};
+
+class KalmanFilter {
+   private:
+    double R;
+    double Q;
+    double P;
+    double U_hat;
+    double H;
+
+   public:
+    KalmanFilter(double process_noise = 10, double measurement_noise = 40,
+                 double initial_estimate = 0)
+        : Q(process_noise),
+          R(measurement_noise),
+          P(0),
+          U_hat(initial_estimate),
+          H(1.0) {}
+
+    double update(double U);
+};
--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,94 @@
 #include <WiFi.h>
 #include <Wire.h>
 
+#include <cmath>
+
 #include "Config.h"
 #include "Globals.h"
-#include "OV7670.h"
+#include "KalmanFilter.h"
 #include "WheelControl.h"
 #include "esp_log.h"
 
 websockets::WebsocketsClient wsClient;
-HTTPClient client;
 
 INA219_WE ina;
 MPU6050 mpu;
-OV7670 camera;
 WheelControl wheels;
 
-TaskHandle_t movementTaskHandle = NULL;
-TaskHandle_t cameraTaskHandler = NULL;
+volatile uint32_t lastInterruptTime = 0;
+volatile uint8_t lastDirection = -1;
 
-int16_t ax, ay, az, gx, gy, gz;
-float currentAngle = 0;
+volatile State state;
 
-void takeImageTask(void *pvParameters);
-void sensorTask(void *pvParameters);
-void movementTask(void *pvParameters);
+void IRAM_ATTR hallSensorISR();
+void wsPolling(void *pvParameters);
+void setYawTask(void *pvParameters);
+void setDistanceTask(void *pvParameters);
+
+void connectToWifi();
+void connectToServer();
+
+void setup() {
+    Serial.begin(USB_SPEED);
+    wheels.attach(IN1, IN2, IN3, IN4);
+    wheels.stop();
+
+    pinMode(HALL_SENSOR, INPUT_PULLUP);
+
+    Wire.begin();
+    ESP_LOGD(mainLogTag, "Memory free: %d", ESP.getFreeHeap());
+
+    if (!ina.init()) {
+        ESP_LOGE(mainLogTag, "INA219 initialization failed!");
+        return;
+    }
+    ESP_LOGI(mainLogTag, "INA219: Successful");
+    connectToWifi();
+    connectToServer();
+
+    mpu.initialize();
+    mpu.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
+    ESP_LOGI(mainLogTag, "Calibrate MPU (20)");
+    mpu.CalibrateGyro(20);
+    mpu.CalibrateAccel(20);
+    ESP_LOGI(mainLogTag, "MPU6050: Successful");
+
+    byte *echoPins = new byte[2]{FRONT_ECHO, SIDE_ECHO};
+    HCSR04.begin(TRIG, echoPins, 2);
+    attachInterrupt(digitalPinToInterrupt(HALL_SENSOR), hallSensorISR, FALLING);
+    xTaskCreatePinnedToCore(wsPolling, "ws", 8192, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(setYawTask, "yt", 8192, NULL, 1, NULL, 1);
+    xTaskCreatePinnedToCore(setDistanceTask, "dt", 8192, NULL, 1, NULL, 1);
+}
+
+String getSensorData() {
+    float busVoltage, current_mA;
+    busVoltage = ina.getBusVoltage_V();
+    current_mA = ina.getCurrent_mA();
+
+    return "vol:" + String(busVoltage) + ",cur:" + String(current_mA) +
+           ",df:" + String(state.distanceFront) +
+           ",ds:" + String(state.distanceSide) + ",ang:" + String(state.angle) +
+           ",ts:" + String(millis()) + ",dh:" + String(state.distanceHall);
+}
+
+void wsPolling(void *pvParameters) {
+    long previousTime = 0;
+    while (true) {
+        long currentTime = millis();
+        if (wsClient.available()) {
+            wsClient.poll();
+            if (state.distanceHall != 0 or millis() - previousTime > 1000) {
+                String data = getSensorData();
+                wsClient.send(data);
+                previousTime = millis();
+                state.distanceHall = 0;
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(10));
+    }
+    vTaskDelete(NULL);
+}
 
 void onEventsCallback(websockets::WebsocketsEvent event, String data) {
     using websockets::WebsocketsEvent;
@@ -45,21 +110,22 @@ void onEventsCallback(websockets::WebsocketsEvent event, String data) {
 void onMessageCallback(websockets::WebsocketsMessage message) {
     if (message.data() == "start") {
         ESP_LOGI(mainLogTag, "Machine started");
-        if (movementTaskHandle != NULL) {
-            vTaskResume(cameraTaskHandler);
-            vTaskResume(movementTaskHandle);
-        }
     } else if (message.data() == "stop") {
         ESP_LOGI(mainLogTag, "Machine stopped");
-        if (movementTaskHandle != NULL) {
-            vTaskSuspend(movementTaskHandle);
-            vTaskSuspend(cameraTaskHandler);
-            wheels.stop();
-        }
     } else if (message.data() == "suspend") {
         ESP_LOGI(mainLogTag, "Machine suspended");
     } else if (message.data() == "resume") {
         ESP_LOGI(mainLogTag, "Machine resumed");
+    } else if (message.data() == "remote_forward") {
+        wheels.forward();
+    } else if (message.data() == "remote_backward") {
+        wheels.backward();
+    } else if (message.data() == "remote_left") {
+        wheels.left();
+    } else if (message.data() == "remote_right") {
+        wheels.right();
+    } else if (message.data() == "remote_stop") {
+        wheels.stop();
     }
 }
 
@@ -87,161 +153,61 @@ void connectToWifi() {
     ESP_LOGI(mainLogTag, "Connected to WiFi");
 }
 
-void setup() {
-    Serial.begin(USB_SPEED);
-    wheels.attach(IN1, IN2, IN3, IN4);
-    wheels.stop();
-
-    Wire.begin();
-    ESP_LOGD(mainLogTag, "Memory free: %d", ESP.getFreeHeap());
-
-    if (!ina.init()) {
-        ESP_LOGE(mainLogTag, "INA219 initialization failed!");
+void IRAM_ATTR hallSensorISR() {
+    ESP_LOGI(mainLogTag, "Interrupt by Hall");
+    lastDirection = wheels.direction;
+    if (wheels.direction == wheel_direction::left or
+        wheels.direction == wheel_direction::right)
         return;
-    }
-    ESP_LOGI(mainLogTag, "INA219: Successful");
-    connectToWifi();
-    connectToServer();
 
-    mpu.initialize();
-    mpu.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
-    ESP_LOGI(mainLogTag, "Calibrate MPU (20)");
-    mpu.CalibrateGyro(20);
-    mpu.CalibrateAccel(20);
-    ESP_LOGI(mainLogTag, "MPU6050: Successful");
-    if (!camera.init(CAM_VSYNC, CAM_HREF, CAM_XCLK, CAM_PCLK, CAM_D0, CAM_D1,
-                     CAM_D2, CAM_D3, CAM_D4, CAM_D5, CAM_D6, CAM_D7)) {
-        ESP_LOGE(mainLogTag, "Camera initialization failed!");
+    uint32_t currentTime = millis();
+    if (currentTime - lastInterruptTime <= 500) return;
+    lastInterruptTime = currentTime;
+    if (lastDirection == wheel_direction::left or
+        lastDirection == wheel_direction::right)
         return;
-    }
-    ESP_LOGI(mainLogTag, "Camera: Successful");
-    byte *echoPins = new byte[2]{FRONT_ECHO, SIDE_ECHO};
-    HCSR04.begin(TRIG, echoPins, 2);
-
-    xTaskCreatePinnedToCore(sensorTask, "sensor", 8192, NULL, 1, NULL, 0);
-    xTaskCreatePinnedToCore(movementTask, "movement", 4096, NULL, 1,
-                            &movementTaskHandle, 1);
-    xTaskCreatePinnedToCore(takeImageTask, "camera", 98304, NULL, 1,
-                            &cameraTaskHandler, 0);
-    if (movementTaskHandle == NULL) {
-        ESP_LOGW(mainLogTag, "movement task: failed to create");
-    } else if (cameraTaskHandler == NULL) {
-        ESP_LOGW(mainLogTag, "camera task: failed to create");
-    } else {
-        vTaskSuspend(cameraTaskHandler);
-        vTaskSuspend(movementTaskHandle);
-        ESP_LOGD(mainLogTag, "Memory free: %d", ESP.getFreeHeap());
-    }
+    if (wheels.direction == wheel_direction::forward)
+        state.distanceHall += WHEEL_DISTANCE_MM;
+    else if (wheels.direction == wheel_direction::backward)
+        state.distanceHall -= WHEEL_DISTANCE_MM;
 }
 
-void takeImageAndSendPostRequest() {
-    camera.oneFrame();
-    client.begin(endpoint);
-    int httpResponseCode =
-        client.sendRequest("POST", camera.frame, camera.frameBytes);
-    if (httpResponseCode > 0) {
-        String payload = client.getString();
-        ESP_LOGI(mainLogTag, "HTTP: [%d] %s", httpResponseCode, payload);
-    } else {
-        ESP_LOGW(mainLogTag, "HTTP-response error");
-    }
-    client.end();
-}
-
-void takeImageTask(void *pvParameters) {
-    ESP_LOGD(mainLogTag, "[TIT]: started");
-    while (true) {
-        ESP_LOGD(mainLogTag, "[TIT]: iter");
-        takeImageAndSendPostRequest();
-        vTaskDelay(pdMS_TO_TICKS(1));
-    }
-    vTaskDelete(NULL);
-}
-
-void sensorTask(void *pvParameters) {
-    ESP_LOGD(mainLogTag, "[sensorTask]: started");
-    float busVoltage, current_mA;
-    uint8_t poolLoop = 0;
-
-    String data;
-    while (true) {
-        busVoltage = ina.getBusVoltage_V();
-        current_mA = ina.getCurrent_mA();
-        double *distances = HCSR04.measureDistanceMm();
-
-        data = "vol:" + String(busVoltage) + ",cur:" + String(current_mA) +
-               ",ang:" + String(currentAngle) + ",df:" + String(distances[0]) +
-               ",ds:" + String(distances[1]);
-
-        poolLoop += 1;
-        if (wsClient.available()) {
-            if (poolLoop == 4) {
-                poolLoop = 0;
-                wsClient.poll();
-            } else {
-                wsClient.send(data);
-            }
-        } else {
-            ESP_LOGD(mainLogTag, "Wi-fi status: %d", WiFi.status());
-            if (WiFi.status() != WL_CONNECTED) {
-                connectToWifi();
-            }
-            wsClient.close();
-            connectToServer();
-        }
-        vTaskDelay(pdMS_TO_TICKS(250));
-    }
-    vTaskDelete(NULL);
-}
-
-void rotate(float angle) {
-    // Alpha - beta filter
-    static float alpha = 0.9;
-    static float beta = 0.0025;
-    float estimatedRate = 0;
-    float dt = 0;
-    float gyroZ = 0;
-    bool once = true;
+void setYawTask(void *pvParameters) {
     long previousTime = 0;
-    currentAngle = 0;
+    double yaw = 0.0;
+    double correctedYaw = 0.0;
+    int16_t ax, ay, az, gx, gy, gz;
     while (true) {
-        mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-        if (once) {
-            wheels.left();
-            once = false;
-        }
-
-        gyroZ = gz / 131;
         long currentTime = millis();
-        dt = (currentTime - previousTime) / 1000.0;
+        mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+        double dt = (currentTime - previousTime) / 1000.0;
         previousTime = currentTime;
+        yaw += static_cast<double>(gz) * dt / GYRO_SENSITIVITY;
 
-        float predictedAngle = currentAngle + estimatedRate * dt;
-        float predictedRate = estimatedRate;
-
-        float residual = gyroZ - predictedRate;
-        currentAngle = predictedAngle + alpha * residual * dt;
-        estimatedRate = predictedRate + beta * residual;
-        ESP_LOGD(mainLogTag, "currentAngle: %f", currentAngle);
-        if (currentAngle >= angle) {
-            wheels.stop();
-            return;
-        }
-
+        correctedYaw = fmod(yaw, 360.0);
+        if (correctedYaw < 0) correctedYaw += 360.0;
+        state.angle = correctedYaw;
         vTaskDelay(pdMS_TO_TICKS(10));
     }
+    vTaskDelete(NULL);
 }
 
-void movementTask(void *pvParameters) {
+void setDistanceTask(void *pvParameters) {
+    KalmanFilter kalmanFront;
+    KalmanFilter kalmanSide;
     while (true) {
-        wheels.forward();
-        vTaskDelay(pdMS_TO_TICKS(2000));
-        wheels.stop();
-        vTaskDelay(pdMS_TO_TICKS(1000));
-        rotate(69.5);
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
+        double *distances = HCSR04.measureDistanceMm();
+        if (distances[0] != -1)
+            state.distanceFront = kalmanFront.update(distances[0]);
+        else
+            state.distanceFront = distances[0];
+        if (distances[1] != -1)
+            state.distanceSide = kalmanSide.update(distances[1]);
+        else
+            state.distanceSide = distances[1];
 
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
     vTaskDelete(NULL);
 }
 
